4dc8429763a0b21512b43c5a43484e9540c20c50
Ethan Buchanan <ethcbuch@ut.utm.edu>
2023-03-28 19:39:21 -0500

    Going good

---
+++ RPGCharacterCreator/WVVM/ViewModel/BioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPGCharacterCreator.Core;
+
+namespace RPGCharacterCreator.WVVM.ViewModel
+{
+    internal class BioViewModel : ObservableObject
+    {
+
+        public BioViewModel()
+        {
+             
+        }
+
+    }
+}
