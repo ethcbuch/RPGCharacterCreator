e90bc35eca81ec512672e4375ccd1171936de1ca
Ethan Buchanan <ethcbuch@ut.utm.edu>
2023-04-02 21:35:58 -0500

    To tomorrow

--- RPGCharacterCreator/WVVM/ViewModel/ClassViewModel.cs
+++ RPGCharacterCreator/WVVM/ViewModel/ClassViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPGCharacterCreator.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace RPGCharacterCreator.WVVM.ViewModel
 {
+
+
+   
     internal class ClassViewModel
     {
+
+        public RelayCommand WarriorButtonCommand { get; set; }
+        public ClassViewModel()
+        {
+            WarriorButtonCommand = new RelayCommand(o =>
+            {
+                 = homeVM;
+
+            });
+        }
+        
+
     }
 }
